4d76ae9013213f0f5ee6225d8a994520ac4a2e6c
1234Sumuu <sumusumaiya74@gmail.com>
2023-04-11 14:45:20 +0600

    typescript 1st program

---
+++ types.ts
@@ -0,0 +1,9 @@
+// console.log("Welcome to the first progtam of Type Script");
+function subscribe(){
+    for(var i = 0; i < 5; i++){
+        console.log(i);
+    }
+    console.log("Outside of the Block!" + i);
+}
+
+subscribe();
