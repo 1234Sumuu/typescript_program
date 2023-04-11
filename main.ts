58b68caf2140a1132d05ac1f367f44df2aa69242
1234Sumuu <sumusumaiya74@gmail.com>
2023-04-11 14:45:59 +0600

    typescript Type

---
+++ main.ts
@@ -0,0 +1,19 @@
+// let myStr: string;
+// myStr = "Welcome to type script first program";
+
+// let myBool : boolean;
+// myBool = true;
+
+// let myAnyType : any;
+// myAnyType = "Kuchu kuchu";
+
+// let arrStr : string[];
+// arrStr = ["Hello", "World"];
+
+// let arrNumber : number[];
+// arrNumber = [1,2,3,4,5];
+
+let anyType;
+anyType = "Any type of Data";
+
+console.log(anyType);
